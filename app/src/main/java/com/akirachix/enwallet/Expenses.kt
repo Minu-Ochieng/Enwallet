fd7c524f0f84da7a0236e0e467debfc2fca5f57a
Minu-Ochieng <minuochieng@gmail.com>
2024-07-08 08:55:47 +0300

    added files

---
+++ app/src/main/java/com/akirachix/enwallet/Expenses.kt
@@ -0,0 +1,10 @@
+package com.akirachix.enwallet
+
+import androidx.annotation.IntegerRes
+
+data class Expenses(
+    var name :String,
+    var price :Int,
+    var date :String,
+
+)
--- app/src/main/java/com/akirachix/enwallet/MainActivity.kt
+++ app/src/main/java/com/akirachix/enwallet/MainActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akirachix.enwallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvEpenses.layoutManager = LinearLayoutManager(this)
+        displayContacts()
+    }
+
+    fun displayContacts() {
+        val expense1 = Expenses("Divedends",24000,"4th July 2024")
+        val expense2 = Expenses("Divedends",24000,"4th July 2024")
+        val expense3 = Expenses("Divedends",24000,"4th July 2024")
+        val expense4 = Expenses("Divedends",24000,"4th July 2024")
+        val expense5 = Expenses("Divedends",24000,"4th July 2024")
+        val expense6 = Expenses("Divedends",24000,"4th July 2024")
+        val expense7 = Expenses("Divedends",24000,"4th July 2024")
+        val expense8 = Expenses("Divedends",24000,"4th July 2024")
+        val expense9 = Expenses("Divedends",24000,"4th July 2024")
+        val expense10 = Expenses("Divedends",24000,"4th July 2024")
+
+
+
+        val contactsList =
+            listOf(expense1,expense2,expense3,expense4,expense5,expense6,expense7,expense8,expense9,expense10)
+        val expenceAdapter = ExpensesAdapter(contactsList)
+        binding.rvEpenses.adapter = expenceAdapter
     }
 }
